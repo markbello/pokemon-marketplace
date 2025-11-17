c34096788350b849223ac27b637950fd1848f76a
Mark Bello <mark@markbello.dev>
2025-11-17 15:57:05 -0500

    [PM-27][FIX] cleanup migrations

--- prisma/migrations/20250106000001_add_stripe_account_id/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "stripeAccountId" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_stripeAccountId_key" ON "User"("stripeAccountId");
-
--- CreateIndex
-CREATE INDEX "User_stripeAccountId_idx" ON "User"("stripeAccountId");
-
